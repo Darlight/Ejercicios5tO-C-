e89ceb4b4a116d71204f9a93e44443c39ddedc2d
Darlight <andres.perdomo09@gmail.com>
2017-01-24 12:02:34 -0600

    casi terminando el ejercicio

--- password.py
+++ password.py
@@ -1,6 +1,19 @@
-def crear_txt(lista):
-	archivo = open('contrasenas.txt','w')
+import random 
+def crear_txt(nombre):
+	archivo = open(nombre,'w')
 	archivo.close()
 
-def crear_contraseno(cantidad,longitud):
+def crear_contrasena(cantidad,longitud,lista,nombre):
+	contra = ""
+	for i in range(cantidad)
+		for i in range(longitud):
+			x = random.randint(0,62)
+			contra += lista[x]
+		archivo = open(nombre,'a')
+		archivo.write(contra,"\n")
+		contra = ""
+
+
+
+
 	
