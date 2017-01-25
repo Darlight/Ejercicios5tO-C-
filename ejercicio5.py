4aed895f1a096792630f9533b22247e5f903686d
Darlight <andres.perdomo09@gmail.com>
2017-01-25 09:05:48 -0600

    ejercicio casi casci completo

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,9 +1,16 @@
+from password import crear_txt, crear_contrasena,leer_txt, escribir_contrasena
+import random
 lista_passwords=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
 print("Bienvenido")
 nombre = input("Ingrese el nombre del archivo(al terminar de escribir su nombre deseado, porfavor terminarlo con .txt): ")
+nombre_archivo = crear_txt(nombre)
 print("Su archivo se creara con el ", nombre)
 longitud = int(input("Ingrese la longitud de la contasena: "))
 cantidad = int(input("Ingrese la cantidad de contrasenas deseadas: "))
+passwords = crear_contrasena(cantidad,longitud,lista_passwords,nombre)
+escribir_contrasena(passwords,nombre)
+
+print(leer_txt(nombre))
 
 
 
--- password.py
+++ password.py
@@ -2,13 +2,28 @@ import random
 def crear_txt(nombre):
 	archivo = open(nombre,'w')
 	archivo.close()
+	return nombre
 
 def crear_contrasena(cantidad,longitud,lista,nombre):
 	contras = []
-	for i in range(cantidad)
+	clave = ""
+	for i in range(cantidad):
 		for i in range(longitud):
-			x = random.randint(0,62)
-			contras.append(lista[x])
+			x = random.randint(0,61)
+			clave += (lista[x])
+			contras.append(clave)
+	return contras
 
+def escribir_contrasena(contras,nombre):
+	archivo = open(nombre,'a')
+	for i in contras:
+		archivo.write(i+"\n")
+	archivo.close()
+	return "Ok"
 
+def leer_txt(nombre):
+	archivo = open(nombre,'r') 
+	lineas = archivo.readlines()
+	archivo.close()
+	return lineas
 	
