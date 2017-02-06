93002ac8f462f0842ced7612710d39a2707038d2
Darlight <andres.perdomo09@gmail.com>
2017-02-06 17:59:59 -0600

    continuando el ejercicio

--- Menu_navegador.py
+++ Menu_navegador.py
@@ -4,32 +4,38 @@ from Tab_class import Tab
 print("Bienvenido \n")
 seguir = 0
 while seguir not in range(1, 9):
- print("""	
+	print("""	
 	1. Abrir Navegador
 	2. Crear un Tab 
- 	2. Cambiar URL de Tab 
-  	3. Cerrar Tab 
-   	4. Cerrar todos los tabs 
-   	5. Mostrar mis tabs 
-   	6. Guardar Tabs
-	7. Salir       """  )
-  seguir = int(input("Que opción desea?: "))
+ 	3. Cambiar URL de Tab 
+  	4. Cerrar Tab 
+   	5. Cerrar todos los tabs 
+   	6. Mostrar mis tabs 
+   	7. Guardar Tabs
+	8. Salir       """  )
+	seguir = int(input("Que opcion desea?: "))
 	if seguir == 1:
 		id_nombre = input("Escriba el nombre del navegador: ")
 		mi_navegador = Navegador(id_nombre)
 
-	if seguir == 2:
+	elif seguir == 2:
+		id_tab = input("Escriba el nombre del tab: ")
+		id_url = input("Ingrese su URL: ")
+		mi_tab = Tab(id_tab,id_url)
+		mi_navegador.tabs.append(mi_tab)
+		mi_tab = ""
 
-	if seguir == 3:
+	elif seguir == 3:
+		id_t
 
-	if seguir == 4:
+	elif seguir == 4:
 
-	if seguir == 5:
+	elif seguir == 5:
 
-	if seguir == 6:
+	elif seguir == 6:
 
-	if seguir == 7:
+	elif seguir == 7:
 
-	if seguir == 8:
+	elif seguir == 8:
 		
 
