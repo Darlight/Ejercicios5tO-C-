8655cb4e684e58c771fdfa4723bd2922f0581617
Darlight <andres.perdomo09@gmail.com>
2017-05-22 09:12:05 -0600

    Crear cajitas con diferentes colores
    Use Google

---
+++ academico/javascript/crear_cajitas.js
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////
+var buttoncreate = document.getElementById("create");
+var quantity = document.getElementById("quantity");
+var color = document.getElementById("color");
+
+buttoncreate.onclick = function	(){
+	var res = "";
+	
+
+
+
+
+}
+
+
+//////////////////////////////////////////////////////
+
+
+
+
+
