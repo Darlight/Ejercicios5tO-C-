4e87895bd2d33a1485c3f3009e080f7745b535f0
Darlight <andres.perdomo09@gmail.com>
2017-05-23 12:11:27 -0600

    Terminar el proyecto

--- academico/javascript/crear_cajitas.js
+++ academico/javascript/crear_cajitas.js
@@ -1,14 +1,14 @@
 //////////////////////////////////////////////////////
 var buttoncreate = document.getElementById("create");
-var quantity = document.getElementById("quantity");
 var color = document.getElementById("color");
-
+var contenedor = document.getElementById("contenedor");
 buttoncreate.onclick = function	(){
-	var res = "";
-	
-
-
-
+	var quantity = parseInt(document.getElementById("quantity").value)
+	var res = contenedor.innerHTML;
+	for (var i = quantity; i > 0; i--) {
+		res += "<div class ='square' style='background:" + color.value + "'> </div>";
+	}
+	contenedor.innerHTML = res
 
 }
 
