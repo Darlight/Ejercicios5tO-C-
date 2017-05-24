33771ecd9ad245cbf45fec27f6d41863f744f4d3
Darlight <andres.perdomo09@gmail.com>
2017-05-24 17:37:07 -0600

    Pensar mejor en clase, por que en la casa debes estudiar mate

--- academico/javascript/histograma.js
+++ academico/javascript/histograma.js
@@ -1,9 +1,18 @@
 //////////////////////////////////
 var quantity = document.getElementById("quantity");
-var ing_button = document.getElementById("insert");
+var ins_button = document.getElementById("insert");
 var hist_button = document.getElementById('hist');
 var container = document.getElementById("container");
 var list = [];
+///////////////quantity.value = function(number){
+	///if (quantity.value < 0 or quantity.value > 100){
+		///window.alert(" Su valor es menor a 0 o mayor a 100. Pruebe otra vez")
+
+//	}
+//}
+/////////////////////////////////////////////////
+
+
 
 quantity.onkeyup = function(e){
 	if(e.which == 13) {
@@ -12,9 +21,18 @@ quantity.onkeyup = function(e){
 
 }
 
-ing_button.onclick = function(){
+ins_button.onclick = function(){
+	list.push(parseInt(quantity.value))
 }
 
+hist_button.onclick = function(){
+	for (var i = 0; i >= 0; i++) {
+		if (list[i] <= 0 and list[i] >=	10) {
+
+
+		} 
+	}
+}
 
 
 
