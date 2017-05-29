a7b9405b1c806d43509f4b114adff17f99fa0ec3
Darlight <andres.perdomo09@gmail.com>
2017-05-29 09:10:53 -0600

    Quitar el numero en el input

--- academico/javascript/histograma.js
+++ academico/javascript/histograma.js
@@ -3,36 +3,46 @@ var quantity = document.getElementById("quantity");
 var ins_button = document.getElementById("insert");
 var hist_button = document.getElementById('hist');
 var container = document.getElementById("container");
+var list_html = document.getElementById("list")
 var list = [];
-///////////////quantity.value = function(number){
-	///if (quantity.value < 0 or quantity.value > 100){
-		///window.alert(" Su valor es menor a 0 o mayor a 100. Pruebe otra vez")
 
-//	}
-//}
-/////////////////////////////////////////////////
 
+function isItValid(value){
+	return value > 0 || value < 100
+}
+
+function addNumberOnList(value){
+	if (isItValid(value)){
+		 list.push(parseInt(value))
+	}
+	else{
+		 windows.alert("Error 504. \n Ingrese un numero mayor que 0 o menor que 100 ")
+	}
+	
+}
 
 
 quantity.onkeyup = function(e){
 	if(e.which == 13) {
-		list.push(parseInt(quantity.value))
+		addNumberOnList(quantity.value);
+		list_html.innerHTML = list.join("/");
 	}
 
 }
 
 ins_button.onclick = function(){
-	list.push(parseInt(quantity.value))
+	addNumberOnList(quantity.value);
+	list_html.innerHTML = list.join("/");
 }
 
-hist_button.onclick = function(){
-	for (var i = 0; i >= 0; i++) {
-		if (list[i] <= 0 and list[i] >=	10) {
+// hist_button.onclick = function(){
+// 	for (var i = 0; i >= 0; i++) {
+// 		if (list[i] <= 0 and list[i] >=	10) {
 
 
-		} 
-	}
-}
+// 		} 
+// 	}
+// }
 
 
 
